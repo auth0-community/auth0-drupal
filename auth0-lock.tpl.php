5f091291c2fbcd0bf728a31348e92833ed335e56
Rob Miroballi <rmiroballi@gmail.com>
2017-01-11 11:01:45 -0600

    Make the link bold and 1.5 font-size

--- auth0-lock.tpl.php
+++ auth0-lock.tpl.php
@@ -1,1 +1,1 @@
-<div id="auth0-login-form" style=" min-height: 440px;"><?php print $login_link ?></div>
+<div id="auth0-login-form" style=" min-height: 440px;"><b sytle="font-size: 1.5em"><?php print $login_link ?></b></div>
