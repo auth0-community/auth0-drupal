771f49ceb2b304d15c3835a83a9c9f20734f57d4
Andreas Koepke <andreas.koepke@businessnews.com.au>
2018-11-02 14:37:53 +0800

    Created settings helper function to standardise loading of common information from Drupal variables. Move parsing of JWT into its own function. Remove return from drupal_goto() function calls, not required. Fix wrong usage of watchdog function. Remove variables from t() function call. Fix issue in auth0_login_auth0_user where $id_token was used for Management class request, needs a specific management token for this. Changes to settings form to support customisation and new features   - Widget CDN URL   - Configuration Base URL for new lock widget   - Login CSS removed (not supported and can be overriden in Auth0 Management)   - Customise login link text Remove template_preprocess_auth0_lock code, moved to auth0_block_view and helper function _auth0_get_params(). Change caching of Lock Widget block. With cache_global the block did not display if the block was generated for someone who was logged in. Remove lock.show(); from auth0.lock.js as not everyone will want the box displaying all the time. Coding standards (comments, camel case, single line if statement, mix of short/long array syntax).

--- auth0.lock.js
+++ auth0.lock.js
@@ -1,3 +1,8 @@
+/**
+ * @file
+ *   Initialise the Lock Widget from Auth0.
+ */
+
 (function ($) {
   /**
    * Attach the Auth0 Lock widget to the login form.
@@ -5,10 +10,7 @@
   Drupal.behaviors.password = {
     attach: function (context, settings) {
       $('#auth0-login-form', context).once(function() {
-
         var lock = new Auth0Lock(settings.auth0.client_id, settings.auth0.domain, settings.auth0.options);
-
-        lock.show();
       })
     }
   }
